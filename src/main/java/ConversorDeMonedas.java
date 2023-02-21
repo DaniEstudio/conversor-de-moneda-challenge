c96648a15e6df9363f947be92ea279b53e5b6426
DaniEstudio <daniela.gavilan05@gmail.com>
2023-02-21 11:46:11 -0300

    aplicacion funcional, conversion peso a dolar funcionando

--- src/main/java/ConversorDeMonedas.java
+++ src/main/java/ConversorDeMonedas.java
@@ -1,58 +1,65 @@
 import javax.swing.JOptionPane;
 
 public class ConversorDeMonedas {
+	private static final double PESO_A_DOLAR = 0.0052;
 
 	public static void main(String[] args) {
-		String opcionElegida = mensaje();
-		conversion(opcionElegida);
+		String opcionElegida = menu();
+		double cantidad = getCantidad();
+		double resultado = conversion(opcionElegida, cantidad);
+		mostrarTotal(opcionElegida, cantidad, resultado);
+	}
+	public static void mostrarTotal(String opcion, double cantidad, double resultado) {
+		String mensaje = cantidad + " pesos equivale a " + resultado + " dólares";
+		JOptionPane.showMessageDialog(null, mensaje);
 	}
 
-	public static String mensaje() {
-	    String[] elegirPesosA = { "Elegir una Opcion", "De Peso a Dólar", "De Peso a Euro", "De Peso a Libras",
-	            "De Peso a Yen", "De Peso a Won Coreano", "De Dólar a Peso", "De Euro a Peso", "De Libra a Peso", "De Yen a Peso", "De Won Coreano a Peso"};
+	public static String menu() {
+		String[] listaPesoA = { "---", "De Peso a Dólar"};
 
-	    String getOpcionesPesosA = (String) JOptionPane.showInputDialog(null, "¿Qué conversion necesita hacer?",
-	            "Conversor", JOptionPane.QUESTION_MESSAGE, null, elegirPesosA, elegirPesosA[0]);
+		// Dialogo
+		String getOpcionPesoA = (String) JOptionPane.showInputDialog(null, "¿Qué conversión necesita hacer?",
+				"Conversor", JOptionPane.QUESTION_MESSAGE, null, listaPesoA, listaPesoA[0]);
+		
+		while(!opcionSeleccionadaValida(getOpcionPesoA)) {
+			getOpcionPesoA = (String) JOptionPane.showInputDialog(null, "Porfavor seleccione una opcion",
+					"Conversor", JOptionPane.WARNING_MESSAGE, null, listaPesoA, listaPesoA[0]);
+		}
+		
+		return getOpcionPesoA;
 
-	    return getOpcionesPesosA;
+	}
+	//Validador
+	public static boolean opcionSeleccionadaValida(String opcion) {
+		return opcion != "---";
 	}
 	
-	public static void conversion(String opcion) {
-	    double cantidad = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad a convertir: "));
-	    double resultado = 0;
-
-	    if (opcion.equals("De Peso a Dólar")) {
-	        resultado = Math.round(cantidad * 192.307692) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " pesos equivale a " + resultado + " dólares");
-	    } else if (opcion.equals("De Peso a Euro")) {
-	        resultado = Math.round(cantidad * 3.90625) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " pesos equivale a " + resultado + " euros");
-	    } else if (opcion.equals("De Peso a Libras")) {
-	        resultado = Math.round(cantidad * 3.378378378) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " pesos equivale a " + resultado + " libras");
-	    } else if (opcion.equals("De Peso a Yen")) {
-	        resultado = Math.round(cantidad * 4761.90476) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " pesos equivale a " + resultado + " yenes");
-	    } else if (opcion.equals("De Peso a Won Coreano")) {
-	        resultado = Math.round(cantidad * 526.315789) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " pesos equivale a " + resultado + " wones coreanos");
-	    } else if (opcion.equals("De Dólar a Peso")) {
-	        resultado = Math.round(cantidad * 192.307692) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " dólares equivale a " + resultado + " pesos");
-	    } else if (opcion.equals("De Euro a Peso")) {
-	        resultado = Math.round(cantidad * 25.6) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " euros equivale a " + resultado + " pesos");
-	    } else if (opcion.equals("De Libra a Peso")) {
-	        resultado = Math.round(cantidad * 29.6) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " libras equivale a " + resultado + " pesos");
-	    } else if (opcion.equals("De Yen a Peso")) {
-	        resultado = Math.round(cantidad * 0.21) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " yenes equivale a " + resultado + " pesos");
-	    } else if (opcion.equals("De Won Coreano a Peso")) {
-	        resultado = Math.round(cantidad * 0.019) / 100.0;
-	        JOptionPane.showMessageDialog(null, cantidad + " wones coreanos equivale a " + resultado + " pesos");
-	    }
+	public static double getCantidad() {
+		String input = JOptionPane.showInputDialog("Ingrese la cantidad a convertir");
+		double cantidad = Double.parseDouble(input);
+		while(!numIngresadoValido(cantidad)) {
+			input = JOptionPane.showInputDialog(null, "Por favor ingrese una cantidad válida.", "Error", JOptionPane.ERROR_MESSAGE);
+			cantidad = Double.parseDouble(input);
+		}
+		return cantidad;
 	}
-
+	
+	//Validador
+	public static boolean numIngresadoValido(double valor) {
+		return valor > 0;
+	}
+	
+	public static double conversion(String opcion, double cantidad) {
+		double total = 0;
+		
+		if(opcion.equals("De Peso a Dólar")) {
+			total = pesoADolar(cantidad);
+		}
+		return total;
+	}
+	
+	public static double pesoADolar(double cantidad) {
+		return cantidad * PESO_A_DOLAR;
+	}
 
 }
